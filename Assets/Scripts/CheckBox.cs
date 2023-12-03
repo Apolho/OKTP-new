9c9e7b5000a991747c44557eb122a2b3734f709a
Apolho <jjhern2017@gmail.com>
2023-12-03 14:43:57 -0500

    Checkboxes now worl

--- Assets/Scripts/CheckBox.cs
+++ Assets/Scripts/CheckBox.cs
@@ -9,29 +9,60 @@ using UnityEngine.SceneManagement;
 public class CheckBox : MonoBehaviour
 {
     public Image[] checkBox;
-    public int amountOfMandates;
-    public GameObject feedBack;
-
+    public TextMeshProUGUI feedBack;
+    private bool allGreen;
+    private bool allWhite;
+    public GameObject feedBackButton;
     public void Continue()
     {
+        allGreen = true;
+        allWhite = true;
+        
         for (int i = 0; i < checkBox.Length; i++)
         {
-            if (checkBox[i].GetComponent<Image>().color == Color.white)
+            if (checkBox[i].GetComponent<Image>().color == Color.white && checkBox[i].CompareTag("isMandate"))
             {
-                feedBack.SetActive(true);
+                //feedBack.SetActive(true);
                 Debug.Log("Wrong");
-                return;
+                allGreen = false;
             }
-            else if (checkBox[amountOfMandates].GetComponent<Image>().color == Color.green)
+            if (checkBox[i].GetComponent<Image>().color == Color.green && checkBox[i].CompareTag("isNotMandate"))
             {
-                //SceneManager.LoadScene("Learn More");
-                Debug.Log("Right");
+                Debug.Log("green");
+                allWhite = false;
             }
-            else
-            {
-                return;
-            }
+            
         }
+
+        if (allGreen == false && allWhite == false)
+        {
+            //make message say they chose incorrect ones and missed correct ones
+            feedBack.text =
+                "Hmm... I don't think we are allowed to do all of these. \n On top of that, you may have missed a few of our actual responsibilities." +
+                "\n Double check your answers and see what's wrong!";
+            feedBackButton.SetActive(true);
+        }
+        else if(allGreen == false && allWhite == true)
+        {
+            //message says they missed correct ones
+            feedBack.text = "Hmm... I think you may have missed a few mandates." +
+            "\n Double check your answers and see what's wrong!";
+            feedBackButton.SetActive(true);
+
+        }
+        else if (allGreen == true && allWhite == false)
+        {
+            //message says they chose incorrect ones
+            feedBack.text = "Hmm... I don't think you are allowed to do all of these." +
+                            "\n Double check your answers and see what's wrong!";
+            feedBackButton.SetActive(true);
+
+        }
+        else if (allGreen == true && allWhite == true)
+        {
+            SceneManager.LoadScene("Learn More");
+        }
+
     }
     public void ChangeColor()
     {
